946780e9522a53e510622ef946aade5324c6b111
Nikola Stojanović <153657652+Stojanovic94@users.noreply.github.com>
2025-01-29 10:50:26 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,16 @@
-// Modal kontrola
 const modal = document.getElementById("productModal");
 const openModalBtn = document.getElementById("addProductBtn");
 const closeModalBtn = document.querySelector(".close-btn");
 const productForm = document.getElementById("productForm");
 
-// Otvaranje modala
 openModalBtn.addEventListener("click", () => {
     modal.style.display = "flex";
 });
 
-// Zatvaranje modala
 closeModalBtn.addEventListener("click", () => {
     modal.style.display = "none";
 });
 
-// Dodavanje proizvoda
 productForm.addEventListener("submit", (e) => {
     e.preventDefault(); // Sprečava podrazumevano ponašanje
     const naziv = document.getElementById("naziv").value;
@@ -29,7 +25,7 @@ productForm.addEventListener("submit", (e) => {
     .then((response) => response.text())
     .then((data) => {
         alert(data); // Prikazuje poruku iz `add_product.php`
-        modal.style.display = "none"; // Zatvara modal
-        location.reload(); // Osvežava stranicu
+        modal.style.display = "none";
+        location.reload();
     });
 });
