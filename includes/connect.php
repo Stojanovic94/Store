d4eea6747cd4ca6780d30224f4536f4e944e9791
Nikola Stojanović <153657652+Stojanovic94@users.noreply.github.com>
2025-01-29 10:51:30 +0100

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,13 +1,11 @@
 <?php
-$servername = "localhost";  // Ako koristite lokalni server, koristite localhost
-$username = "root";         // Korisničko ime je 'root' u XAMPP-u (osim ako niste postavili lozinku)
-$password = "";             // Lozinka je prazna po defaultu u XAMPP-u
-$dbname = "prodavnica";     // Ime baze podataka koju ste kreirali
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "prodavnica";
 
-// Kreiranje konekcije
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Provera konekcije
 if ($conn->connect_error) {
     die("Konekcija neuspešna: " . $conn->connect_error);
 }
